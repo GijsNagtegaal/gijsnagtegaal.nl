ca1aa9ee87cdd9b6014ed43b92f533608ed197f2
Gijs <gijs.nagtegaal@hva.nl>
2026-05-04 21:25:59 +0200

    change of dep

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 import express from 'express';
 import { Liquid } from 'liquidjs';
-import { fileURLToPath } from 'url';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+const engine = new Liquid({
+    root: path.resolve(__dirname, 'public/views'), 
+    extname: '.liquid'
+});
+
+app.engine('liquid', engine.express());
+app.set('views', path.join(__dirname, '/views')); 
+app.set('view engine', 'liquid');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const engine = new Liquid();
-app.engine('liquid', engine.express());
-app.set('views', './views');
-app.set('view engine', 'liquid');
-
 app.get('/', async (req, res) => {
+    res.render('index'); 
+});
 
-    res.render('index.liquid', {
-    });
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
